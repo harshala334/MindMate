82887a690c77db838c0e57e4ce05bf19ee50ead8
Harshala <mharshala334@gmail.com>
2025-09-27 11:33:13 +0530

    added 'import toast...' to RegisterPage.jsx'

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
 import { useAuth } from '../contexts/AuthContext';
 import LoadingSpinner from '../components/LoadingSpinner';
 
